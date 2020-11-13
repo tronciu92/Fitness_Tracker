5c64f9edc10b10932bd83c9b1a61a664b21bd6ee
Vasile <tronciu92@gmail.com>
2020-11-12 18:54:39 -0800

    server

--- server.js
+++ server.js
@@ -15,7 +15,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/Fitness-Workout',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
+  
 
 require("./routes/api-routes.js")(app);
 
